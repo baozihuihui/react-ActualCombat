b2de9a0e2c70aa25e6df7da86f06164566909c2b
FattyCat <731493610@qq.com>
2021-03-01 15:00:26 +0800

    add React.creatElement

--- hello-react/src/index.jsx
+++ hello-react/src/index.jsx
@@ -1,4 +1,15 @@
-import * as ReactDOM from "react-dom";
-import App from "./App";
+// import * as ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "./react/React";
+import ReactDOM from "./react/ReactDOM";
+
+const jsx = (
+  <div>
+    <p>react</p>
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
